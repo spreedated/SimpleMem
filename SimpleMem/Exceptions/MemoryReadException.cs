816c5fd22405514f971f709cfcb8d7398289fbe4
spreedated <21370839+spreedated@users.noreply.github.com>
2024-02-01 20:31:16 +0100

    encapsulated exepctions

---
+++ SimpleMem/Exceptions/MemoryReadException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleMem.Exceptions
+{
+    /// <summary>
+    ///  Thrown when memory read operations have failed.
+    ///  Intended to be used as a support tool for hard-to-debug issues.
+    /// </summary>
+    public class MemoryReadException : Exception
+    {
+        public MemoryReadException(string message) : base(message) { }
+
+        public MemoryReadException(uint error) : base($"Error code {error} {ErrorCodes.CodeLookup.GetValueOrDefault(error)}") { }
+
+        public MemoryReadException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ SimpleMem/Exceptions/MemoryWriteException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleMem.Exceptions
+{
+    /// <summary>
+    ///  Thrown when memory write operations have failed.
+    ///  Intended to be used as a support tool for hard-to-debug issues.
+    /// </summary>
+    public class MemoryWriteException : Exception
+    {
+        public MemoryWriteException() { }
+
+        public MemoryWriteException(string message) : base(message) { }
+
+        public MemoryWriteException(uint error) : base($"Error code {error} {ErrorCodes.CodeLookup.GetValueOrDefault(error)}") { }
+
+        public MemoryWriteException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
