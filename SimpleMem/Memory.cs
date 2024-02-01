e712fcc98e2f5e0d27e7b34b8b63f5ef1cf56177
spreedated <21370839+spreedated@users.noreply.github.com>
2024-02-01 19:51:01 +0100

    revised csproj

--- SimpleMem/Memory.cs
+++ SimpleMem/Memory.cs
@@ -1,7 +1,10 @@
-﻿using System.Buffers;
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
